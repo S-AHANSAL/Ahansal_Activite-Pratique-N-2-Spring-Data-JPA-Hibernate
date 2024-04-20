07ba7962cdf095647caca42883e0f19431a24dc0
SALAHEDDINE AHANSAL <salaheddineahansal@gmail.com>
2024-04-20 01:32:51 +0100

    first commit Part 1

---
+++ Activite2_Ahansal_Salaheddine__Part2/src/main/java/ma/fsm/activite2_ahansal_salaheddine__part2/Activite2AhansalSalaheddinePart2Application.java
@@ -0,0 +1,13 @@
+package ma.fsm.activite2_ahansal_salaheddine__part2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activite2AhansalSalaheddinePart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activite2AhansalSalaheddinePart2Application.class, args);
+	}
+
+}
---
+++ Activite2_Ahansal_Salaheddine__Part2/src/test/java/ma/fsm/activite2_ahansal_salaheddine__part2/Activite2AhansalSalaheddinePart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsm.activite2_ahansal_salaheddine__part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activite2AhansalSalaheddinePart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
