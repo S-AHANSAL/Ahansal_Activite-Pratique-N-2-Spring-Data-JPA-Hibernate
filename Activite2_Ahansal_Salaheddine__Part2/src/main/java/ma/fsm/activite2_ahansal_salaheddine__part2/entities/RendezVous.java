35db13842e965b7fcb67f5292b67b809819187c0
SALAHEDDINE AHANSAL <salaheddineahansal@gmail.com>
2024-04-21 18:43:02 +0100

    app Spring Hospital version 3

--- Activite2_Ahansal_Salaheddine__Part2/src/main/java/ma/fsm/activite2_ahansal_salaheddine__part2/entities/RendezVous.java
+++ Activite2_Ahansal_Salaheddine__Part2/src/main/java/ma/fsm/activite2_ahansal_salaheddine__part2/entities/RendezVous.java
@@ -1,5 +1,6 @@
 package ma.fsm.activite2_ahansal_salaheddine__part2.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class RendezVous {
     @Enumerated(EnumType.STRING)
     private StatusRDV status;
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Patient patient;
     @ManyToOne
     private Medecin medecin;
